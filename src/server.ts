4ed14642d8260e8bed82b6fd9e40c36963941ef3
Nalisson Gomes <nalissongm.pro@gmail.com>
2022-05-03 14:42:14 -0300

    fix: volume directory change

--- src/server.ts
+++ src/server.ts
@@ -13,4 +13,4 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(3333, () => console.log("Server is running! ;)"));
+app.listen(3333, () => console.log("Server is running! ;q)"));

b6f494d6b0d1e566b760f18bc0d3848f0d7afaad
Nalisson Gomes <nalissongm.pro@gmail.com>
2022-05-13 14:49:53 -0300

    feat: handle errors

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,10 +1,43 @@
-// import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
 
-// export async function ensureAuthenticated(
-//   request: Request,
-//   response: Response,
-//   next: NextFunction
-// ) {
-//   const uthHeader =
-//   return null;
-// }
+import { AppError } from "../errors/AppError";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "ab848ac9525b3ab34cf8e2d685dd2c3b127c5b4e5068738edf79d66df9b1b0a2"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User does not exists!", 401);
+    }
+
+    next();
+  } catch {
+    throw new AppError("Invalid token!", 401);
+  }
+
+  return null;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import "reflect-metadata";
-
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
+import "express-async-errors";
+import "reflect-metadata";
+import "./shared/container";
+
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
 import "./database";
-
-import "./shared/container";
 
 const app = express();
 
@@ -17,5 +18,18 @@ app.use(express.json());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+// eslint-disable-next-line no-unused-vars
+app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError)
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error -> ${err.message}`,
+  });
+});
 
 app.listen(3333, () => console.log("Server is running!"));

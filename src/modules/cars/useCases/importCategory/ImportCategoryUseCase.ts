f6543e2bd868f7db6172e55739fcc92f95bb4ca7
Nalisson Gomes <nalissongm.pro@gmail.com>
2022-08-09 14:19:24 -0300

    refactor: imports

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -3,7 +3,7 @@ import { Express } from "express";
 import fs from "fs";
 import { inject, injectable } from "tsyringe";
 
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+import { CategoriesRepository } from "@modules/cars/repositories/implementations/CategoriesRepository";
 
 interface IImportCategory {
   name: string;
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import swaggerUi from "swagger-ui-express";
 
 import "express-async-errors";
 import "reflect-metadata";
-import "./shared/container";
+import "@shared/container";
 
-import { AppError } from "./errors/AppError";
+import { AppError } from "@errors/AppError";
+
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -19,7 +20,7 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError)
     return res.status(err.statusCode).json({
